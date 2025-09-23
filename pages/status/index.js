896371caaa396db9514f2cf8589f5a6c9819304f
Bertonni Thiago de Souza Paz <bertonnipaz@gmail.com>
2025-09-23 08:53:03 -0300

    feat: add initial version of `/status` page

---
+++ pages/status/index.js
@@ -0,0 +1,46 @@
+import useSWR from "swr";
+
+async function fetchAPI(key) {
+  const response = await fetch(key);
+  const responseBody = await response.json();
+
+  return responseBody;
+}
+
+export default function StatusPage() {
+  
+  return (
+    <>
+      <h1>Status</h1>
+      <DatabaseStatus />
+    </>
+  );
+}
+
+function DatabaseStatus() {
+  const { data, isLoading } = useSWR("/api/v1/status", fetchAPI, {
+    refreshInterval: 2000
+  });
+
+  let updatedAtText = "Carregando...";
+  let databaseStatusInformation = "Carregando...";
+  
+  if(!isLoading && data) {
+    databaseStatusInformation = (
+      <>
+        <div>Versão: {data.dependencies.database.version}</div>
+        <div>Conexões abertas: {data.dependencies.database.opened_connections}</div>
+        <div>Conexões máximas: {data.dependencies.database.max_connections}</div>
+      </>
+    );
+    updatedAtText = new Date(data.updated_at).toLocaleString("pt-BR");
+  }
+
+  return (
+    <>
+      <div>Última atualização: {updatedAtText}</div>
+      <h2>Database</h2>
+      <div>{databaseStatusInformation}</div>
+    </>
+  )
+}
